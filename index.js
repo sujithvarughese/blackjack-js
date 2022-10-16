469af421702b55b9626aad53c5c6d839a08d12e9
Sujith Varughese <sujithv15@gmail.com>
2022-10-16 07:01:40 -0400

    game functions all working properly

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ let dealerRoll = document.getElementById('dealer-bankroll');
 
 let deck = [2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11];
 let shoe = [];
+
+// shuffles deck, adds into shoe, and loops for number of decks desired in shoe, then shuffles shoe 10 times
 function createNewShoe(numDecks=1) {
+    deck.sort( ()=> Math.random()-0.5);
     for (let i = 0; i < numDecks; i++) {
         for (let j = 0; j < deck.length; j++) {
             shoe.push(deck[j]);
@@ -21,18 +24,21 @@ function createNewShoe(numDecks=1) {
         shoe.sort(() => Math.random() - 0.5);
     }
 }
+// shuffle shoe user desired amount of times
 function shuffle(numShuffles=10){
     for (let i = 0; i < numShuffles; i++) {
         shoe.sort(() => Math.random() - 0.5);
     }
 }
+// dealer and player objects created
 let dealer = {
     name: 'Evil Dealer',
     bankroll: 0,
     score: 0,
     hand: [],
     blackjack: false,
-    bust: false
+    bust: false,
+    ready: false
 }
 let player = {
     name: 'Whale',
@@ -45,73 +51,84 @@ let player = {
     canHit: false,
     hasAce: false
 }
+function loadMoney(playerName, loadAmount) {
+    playerName.bankroll = loadAmount;
+}
 function reloadMoney(playerName, reloadAmount) {
-    playerName.bankroll += reloadAmount
+    playerName.bankroll += reloadAmount;
 }
 
+// checks table for any blackjacks and updates bankrolls if found.
+// if no blackjack found, allows user to choose an action for the player (hit or stay)
 function checkBlackjack(){
     if (dealer.score === 21) {
         dealer.blackjack = true;
-        dealerHand.textContent += dealerHand[1]
     }
     if (player.score === 21) {
         player.blackjack = true;
-        dealerHand.textContent += dealerHand[1]
     }
     if (dealer.blackjack === true && player.blackjack === true) {
         console.log('Push')
         userNotify.textContent = 'Push!'
+        dealerScore.textContent = 'Dealer Score: ' + dealer.score;
+        dealerHand.textContent = 'Dealer Hand: ' + dealer.hand.toString()
+        dealer.ready = true;
     } else if (dealer.blackjack === true) {
         console.log('Dealer blackjack')
         userNotify.textContent = 'Dealer Blackjack'
+        dealerHand.textContent = 'Dealer Hand: ' + dealer.hand.toString()
         player.bankroll -= player.bet;
         dealer.bankroll += player.bet;
+        dealer.ready = true;
     } else if (player.blackjack === true) {
         console.log('Player blackjack')
         userNotify.textContent = 'Player Blackjack! Congratulations!';
+        dealerHand.textContent = 'Dealer Hand: ' + dealer.hand.toString()
         player.bankroll += (player.bet * 1.5);
         dealer.bankroll -= (player.bet * 1.5);
+        dealer.ready = true;
     } else {
-        console.log('Dealer score: ' + dealer.hand[0])
         userNotify.textContent = 'Your score is ' + player.score;
         userNotify2.textContent = 'Hit or stay?'
-        player.canHit = true
+        player.canHit = true;
     }
 }
+
+// deal only permitted after new game is started or hand has completed
 function dealHand() {
-    dealer.hand = [];
-    player.hand = [];
-    dealer.score = 0;
-    player.score = 0;
-    dealer.bust = false;
-    player.bust = false;
-    dealer.blackjack = false;
-    dealer.blackjack = false;
-    player.canHit = false;
+    if (dealer.ready) {
+        dealer.ready = false;
+        userNotify.textContent = 'Press Deal Cards when ready!'
+        userNotify2.textContent = ''
+        clearHands()
 
-    player.hand.push(shoe.pop());
-    playerHand.textContent = 'Player hand: ' + player.hand[0];
+        // cards dealt one by one, beginning with player
+        player.hand.push(shoe.pop());
+        playerHand.textContent = 'Player hand: ' + player.hand.toString();
 
-    dealer.hand.push(shoe.pop());
-    dealerHand.textContent = 'Dealer hand: ' + dealer.hand[0];
-    dealerScore.textContent = 'Dealer score: ' + dealer.hand[0]
+        dealer.hand.push(shoe.pop());
+        dealerHand.textContent = 'Dealer hand: ' + dealer.hand.toString();
+        dealerScore.textContent = 'Dealer score: ' + dealer.hand[0]
 
-    player.hand.push(shoe.pop());
-    playerHand.textContent += " " + player.hand[1];
+        player.hand.push(shoe.pop());
+        playerHand.textContent = 'Player hand: ' + player.hand.toString();
 
-    dealer.hand.push(shoe.pop())
-    dealer.score = dealer.hand[0] + dealer.hand[1];
-    player.score = player.hand[0] + player.hand[1];
+        // dealer's first card and score only shown, while other card hidden until player actions have completed
+        dealer.hand.push(shoe.pop())
+        dealer.score = dealer.hand[0] + dealer.hand[1];
+        player.score = player.hand[0] + player.hand[1];
 
-    playerScore.textContent = 'Player Score: ' + player.score
+        playerScore.textContent = 'Player Score: ' + player.score
 
-    console.log(player.hand)
-    console.log(dealer.hand)
-
-    checkBlackjack();
+        console.log(player.hand)
+        console.log(dealer.hand)
+        // first calls function to check for any blackjacks
+        checkBlackjack();
+    }
 }
 
-function shuffleUp(numDecks=1) {
+// clears all fields before deal
+function clearHands() {
     dealer.hand = [];
     player.hand = [];
     dealer.score = 0;
@@ -123,66 +140,90 @@ function shuffleUp(numDecks=1) {
     player.canHit = false;
     playerHand.textContent = ""
     dealerHand.textContent = ""
+}
+
+// creates shoe, shuffles, wagers placed
+function shuffleUp(numDecks=1) {
+    clearHands()
     createNewShoe(numDecks);
     shuffle();
-    reloadMoney(dealer, 5000)
-    reloadMoney(player, 1000)
+    loadMoney(dealer, 5000)
+    loadMoney(player, 1000)
     userNotify.textContent = 'Press Deal Cards when ready!'
     userNotify2.textContent = ''
     playerRoll.textContent = 'Player Bankroll: ' + player.bankroll
     dealerRoll.textContent = 'Dealer bankroll: ' + dealer.bankroll
+    playerHand.textContent = 'Player Hand: ';
+    playerScore.textContent = "Player Score: ";
+    dealerHand.textContent = 'Dealer hand: ';
+    dealerScore.textContent = 'Dealer score: ';
     player.bet = 25
     betAmount.textContent = 'Player bet: ' + player.bet
+    dealer.ready = true;
 }
 
+// button only allowed when given permission
 function hitMe(){
-    if (player.canHit === true && player.bust === false) {
+    if (player.canHit === true) {
+        player.canHit = false;
         let nextCard = shoe.pop();
         player.hand.push(nextCard);
         player.score += nextCard
-        for (let i = 0; i < player.hand.length; i++) {
-            if (player.hand[i] === 11 && player.score > 21) {
-                player.hand[i] = 1;
-                player.score -= 10;
-            }
+        while (player.score > 21 && player.hand.includes(11)) {     // if player's hand contains an Ace and score goes over 21,
+            player.hand[player.hand.indexOf(11)] = 1;                       // the ace (11) must be changed to a 1
+            player.score -= 10;
         }
-        playerHand.textContent += " " + nextCard
+        playerHand.textContent = 'Player Hand: ' + player.hand.toString()
         playerScore.textContent = "Player Score: " + player.score
         console.log('Your score is ' + player.score)
         userNotify.textContent = 'Your score is ' + player.score
-        if (player.score > 21){
+
+        // if player makes 21 or busts, player is not allowed to make any further action until dealers hand is played
+        if (player.score === 21) {
+            userNotify2.textContent = 'Player has 21!'
+            dealerMove();
+        }
+        else if (player.score > 21){
             player.bust = true;
-            player.canHit = false;
             userNotify2.textContent = 'Player Bust. Ouch!'
             console.log('Player Bust');
+            dealerMove()
         }
         else {
             userNotify2.textContent = 'Hit again?'
+            player.canHit = true;
         }
     }
 }
 
+// only occurs after player moves have completed
 function dealerMove(){
-    if (player.canHit === true && player.bust === false) {
+    if (player.canHit === true) {
         player.canHit = false;
-        let nextCard = 0
+        if (player.bust === false) {
+            userNotify2.textContent = '';
+        }
+        // dealer must draw until minimum 17 score is reached
         while (dealer.score < 17) {
-            nextCard = shoe.pop()
+            let nextCard = shoe.pop()
             dealer.hand.push(nextCard);
             dealer.score += nextCard
-            for (let i = 0; i < dealer.hand.length; i++) {
-                if (dealer.hand[i] === 11 && dealer.score > 21) {
-                    dealer.score -= 10;
-                }
+            // dealer also changes 11 value of ace to 1, if score goes over 21
+            while (dealer.score > 21 && dealer.hand.includes(11)) {
+                dealer.hand[dealer.hand.indexOf(11)] = 1;
+                dealer.score += 10;
             }
-            dealerHand.textContent += " " + nextCard;
-            dealerScore.textContent = "Dealer score: " + dealer.score
+            dealerHand.textContent = 'Dealer Hand: ' + dealer.hand.toString();
+            dealerScore.textContent = 'Dealer score: ' + dealer.score
         }
+        // all possible game outcomes accounted for, and bankroll is properly adjusted
         if (dealer.score > 21) {
             dealer.bust = true;
             console.log('Dealer Bust');
             userNotify.textContent = 'Dealer Bust!'
         }
+        dealerHand.textContent = 'Dealer hand: ' + dealer.hand.toString();
+        dealerScore.textContent = 'Dealer Score: ' + dealer.score;
         if (dealer.bust === false) {
             if (player.bust === true) {
                 player.bankroll -= player.bet;
@@ -212,6 +253,9 @@ function dealerMove(){
             }
         }
     }
+    playerRoll.textContent = 'Player bankroll: ' + player.bankroll;
+    dealerRoll.textContent = 'Dealer bankroll: ' + dealer.bankroll;
+    dealer.ready = true;
 }
 
 
